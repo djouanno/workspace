8623348e9c3d7d7de9d9dcb3397f4392e392ad83
hrougier <rougier.h@gmail.com>
2013-11-25 23:42:31 +0100

    some javadoc

--- sweetsnake/sweetsnake-commons/src/main/java/com/esir/sr/sweetsnake/api/IGuiForServer.java
+++ sweetsnake/sweetsnake-commons/src/main/java/com/esir/sr/sweetsnake/api/IGuiForServer.java
@@ -7,6 +7,8 @@ import com.esir.sr.sweetsnake.dto.GameSessionDTO;
 import com.esir.sr.sweetsnake.dto.PlayerDTO;
 
 /**
+ * This interface represents which methods a Graphic User Interface for the server must be able to provide.<br />
+ * All the methods below are intented to be called by the server according to the events it processed.
  * 
  * @author Herminaël Rougier
  * @author Damien Jouanno
@@ -16,30 +18,36 @@ public interface IGuiForServer
 {
 
     /**
-     * 
+     * This method is called by the server in order to notify the GUI that the server has been started
      */
     void serverStarted();
 
     /**
-     * 
+     * This method is called by the server in order to notify the GUI that the server has been stopped
      */
     void serverStopped();
 
     /**
+     * This method is called by the server in order to notify the GUI to refresh the players list
      * 
      * @param players
+     *            A list containing the DTO representing all the connected players
      */
     void refreshPlayers(List<PlayerDTO> players);
 
     /**
+     * This method is called by the server in order to notify the GUI to refresh the sessions list
      * 
      * @param sessions
+     *            A list containing the DTO representing all the sessions
      */
     void refreshSessions(List<GameSessionDTO> sessions);
 
     /**
+     * This method is called by the server in order to notify the GUI to refresh the requests list
      * 
      * @param requests
+     *            A list containing the DTO representing all the pending requests
      */
     void refreshRequests(List<GameRequestDTO> requests);
 
