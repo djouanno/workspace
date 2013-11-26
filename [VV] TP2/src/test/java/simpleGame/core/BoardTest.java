9b4aac05e75ef5bd4cbfafbf131ad5fb98a704bd
Damien <damien.jouanno@gmail.com>
2013-11-26 12:01:49 +0100

    VV TP2 stubs

--- [VV] TP2/src/test/java/simpleGame/core/BoardTest.java
+++ [VV] TP2/src/test/java/simpleGame/core/BoardTest.java
@@ -1,16 +1,38 @@
 package simpleGame.core;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 
+@RunWith(MockitoJUnitRunner.class)
 public class BoardTest
 {
-    private Board board;
+
+    @Mock
+    private Pawn            currentPawn;
+
+    private ArrayList<Pawn> pawns;
+
+    private Board           board;
 
     @Before
     public void init() {
-        board = new Board(0, 0, 0);
+        board = new Board(2, 8, 8);
+        pawns = new ArrayList<>();
+        pawns.add(mock(Pawn.class));
+        pawns.add(mock(Pawn.class));
+        pawns.add(mock(Pawn.class));
+
+        board.setPawns(pawns);
     }
 
     @Test
@@ -50,12 +72,16 @@ public class BoardTest
 
     @Test
     public void numberOfPawnsTest() {
-
+        assertEquals(board.numberOfPawns(), pawns.size());
     }
 
     @Test
     public void maxGoldTest() {
+        when(pawns.get(0).getGold()).thenReturn(300);
+        when(pawns.get(1).getGold()).thenReturn(500);
+        when(pawns.get(2).getGold()).thenReturn(100);
 
+        assertEquals(board.maxGold(), 500);
     }
 
     @Test
